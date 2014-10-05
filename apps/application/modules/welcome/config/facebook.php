19c0d5051951211882c0d2e55191f89f380ae20a
Tran Phuoc Tien <starshinevietnam@gmail.com>
2014-10-05 18:24:11 +0700

    Update config facebook

--- apps/application/modules/welcome/config/facebook.php
+++ apps/application/modules/welcome/config/facebook.php
@@ -1,8 +1,8 @@
 <?php if (!defined('BASEPATH')) exit('No direct script access allowed');
 
 
-$config['facebook']['api_id'] = '1488714401412382';
-$config['facebook']['app_secret'] = '1c32904a8458d36eb71e9f218fc66a0a';
+$config['facebook']['api_id'] = 'Your APP ID';
+$config['facebook']['app_secret'] = 'Your Secret Key';
 $config['facebook']['redirect_url'] = 'http://birthday-openshift.local/';
 $config['facebook']['permissions'] = array(
     'email',
